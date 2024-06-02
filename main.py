2f1866fac599b3105698bc5067c6c8d9648c549f
Alan-Luc <alanluc9@gmail.com>
2024-06-02 18:31:31 -0400

    mostly finished

--- main.py
+++ main.py
@@ -9,15 +9,33 @@ class Board:
             self.board[i] = None
 
         self.board[3] = Property(3, 200, 50)
-        self.board[7] = Property(7, 400, 100)
-        self.board[13] = Property(13, 800, 350)
+        self.board[7] = Property(7, 300, 75)
+        self.board[13] = Property(13, 350, 110)
+        self.board[19] = Property(19, 400, 150)
+        self.board[23] = Property(23, 500, 170)
+        self.board[31] = Property(31, 700, 300)
+        self.board[35] = Property(35, 800, 400)
+        self.board[27] = Property(27, 550, 250)
+        self.board[15] = Property(15, 400, 150)
+        self.board[29] = Property(29, 600, 290)
+        self.board[38] = Property(38, 100, 600)
 
     def handleProperty(self, player):
         currTile = player.location
         if self.board[currTile] is not None:
             property = self.board[currTile]
-            if property.owner and property.owner is not player:
-                player.money -= property.rent
+            if property.owner:
+                if property.owner is not player:
+                    print(
+                        f"{player.name}, you've landed on {property.owner.name}'s property!\n"
+                        f"You owe them ${property.rent}..."
+                    )
+                    input("Press any button to continue...")
+                    player.money -= property.rent
+                    property.owner.money += property.rent
+                    return
+                else:
+                    return
             else:
                 print(f"\nYou've landed on a property. {property}")
                 choice = input("Would you like to buy this property? \n")
@@ -25,11 +43,11 @@ class Board:
                     print("\nInvalid choice...")
                     choice = input("Would you like to buy this property? \n")
 
-                if player.money <= property.price:
-                    print("You don't have enough money...")
-                    return
-
                 if choice == "y":
+                    if player.money <= property.price:
+                        print("You don't have enough money...")
+                        input("Press any button to continue...")
+                        return
                     player.money -= property.price
                     property.owner = player
                     player.properties.append(property)
@@ -52,7 +70,7 @@ class Player:
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
         diceRoll = dice1 + dice2
-        print(f"You rolled... Dice 1: {dice1}, Dice 2: {dice2}, Total: {diceRoll}")
+        print(f"\nYou rolled... Dice 1: {dice1}, Dice 2: {dice2}, Total: {diceRoll}")
         currTile = (self.location + diceRoll) % 40
         if self.inJail:
             return dice1, dice2, currTile
@@ -63,6 +81,13 @@ class Player:
         return dice1, dice2, currTile
 
     def turn(self, doubles=0):
+        print(
+            f"\nIt's your turn {self.name}!\n"
+            f"Balance: ${self.money}\n"
+            f"Current Tile: {self.location+1}\n"
+            f"Properties: {self.properties}\n"
+        )
+
         if doubles == 3:
             self.location = 8
             self.inJail = True
@@ -81,10 +106,16 @@ class Player:
                     "Choices: {pay, roll, stay}\n"
                 )
             if choice == "pay":
-                self.money -= 50
-                print(f"{self.name} balance is now: ${self.money}")
-                self.inJail = False
-                return
+                if self.money > 50:
+                    self.money -= 50
+                    print(f"{self.name}'s balance is now: ${self.money}")
+                    self.inJail = False
+                    return
+                else:
+                    print("You don't have enough money...")
+                    input("Press any button to continue...")
+                    return
+
             elif choice == "roll":
                 dice1, dice2, _ = self.roll()
                 if dice1 == dice2:
@@ -94,13 +125,8 @@ class Player:
                 return
 
         self.doubles = doubles
-        print(
-            f"It's your turn {self.name}!\n"
-            f"Balance: {self.money}\n"
-            f"Current Tile: {self.location}\n"
-            f"Properties: {self.properties}\n"
-        )
-        roll_prompt = input("Press r to roll!\n")
+
+        roll_prompt = input("Press r to roll! ")
         while roll_prompt != "r":
             print("\nInvalid input!!!")
             roll_prompt = input("Press r to roll!\n")
@@ -108,7 +134,10 @@ class Player:
         dice1, dice2, currTile = self.roll()
         board.handleProperty(self)
 
-        if currTile == 8:
+        if self.money <= 0:
+            return
+
+        if currTile == 20:
             self.inJail = True
 
         if dice1 == dice2:
@@ -131,5 +160,12 @@ class Property:
 
 board = Board()
 p1 = Player("Alan")
-while p1.money > 0:
-    p1.turn(0)
+p2 = Player("Melissa")
+players = [p1, p2]
+
+while players and len(players) > 1:
+    for player in players:
+        player.turn()
+        if player.money <= 0:
+            players.remove(player)
+            print(f"{player.name} is out of money and has lost!")
